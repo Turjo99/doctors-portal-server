ba333971d7419315fa9d45f7cf13d7938507f22d
Turjo99 <imturjo27@gmail.com>
2022-11-16 16:10:48 +0600

    AppointmentOption API Loaded from database

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 //WCEfKrHri06fKFMV
 //doctorsPortal
 const port = process.env.PORT || 5000;
-
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xyxb77f.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -14,14 +15,21 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
-app.use(cors());
-app.use(express.json());
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorsPortal")
+      .collection("appointmentOptions");
+    app.get("/appointmentOption", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 app.get("/", (req, res) => {
   res.send("doctors portal Server Running");
 });
