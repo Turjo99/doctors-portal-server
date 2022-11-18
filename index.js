6ce510e4bc3c7657103a69ed23d9297e2e639d93
Turjo99 <imturjo27@gmail.com>
2022-11-18 13:01:58 +0600

    get filtered booking by email

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
       });
       res.send(options);
     });
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const query = {
